b60e0241acd31b38f2487f28830fbc3122751b35
Andrew Yefymenko <andruefim@gmail.com>
2024-12-15 23:32:57 +0100

    Add transactions model and controller

---
+++ Models/Transaction.cs
@@ -0,0 +1,11 @@
+﻿namespace AngularWithASP.Server.Models;
+
+public class Transaction
+{
+    public long Id { get; set; }
+    public string? Date { get; set; }
+    public string? Category { get; set; }
+    public long Amount {  get; set; }
+    public string? Description { get; set; }
+}
+ 
---
+++ Models/TransactionsContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AngularWithASP.Server.Models;
+
+public class TransactionsContext : DbContext
+{
+    public TransactionsContext(DbContextOptions<TransactionsContext> options)
+        : base(options)
+    { 
+    }
+
+    public DbSet<Transaction> Transactions { get; set; } = null!;
+}
