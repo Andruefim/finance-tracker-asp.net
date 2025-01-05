7ea59487accd7f0d30769224fcba884fda363b5d
Andrew Yefymenko <andruefim@gmail.com>
2025-01-05 19:43:11 +0100

    Build dynamic transaction expressions

--- Expressions/TransactionExpressions.cs
+++ Expressions/TransactionExpressions.cs
@@ -7,16 +7,37 @@ public static class TransactionExpressions
 {
     public static Expression<Func<Transaction, long>> ToAbsoluteAmount()
     { 
-        return t => Math.Abs(t.Amount);
+        //return t => Math.Abs(t.Amount);
+
+        var tParameter = Expression.Parameter(typeof(Transaction), "t");
+        var amountProperty = Expression.Property(tParameter, nameof(Transaction.Amount));
+
+        var absMethod = typeof(Math).GetMethod(nameof(Math.Abs), [typeof(long)]) 
+            ?? throw new InvalidOperationException($"{nameof(Math.Abs)} not found!");
+
+        var absoluteAmount = Expression.Call(absMethod, amountProperty);
+
+        return Expression.Lambda<Func<Transaction, long>>(absoluteAmount, tParameter);
     }
 
     public static Expression<Func<Transaction, bool>> IsExpenseTransaction()
     {
-        return t => t.Amount < 0;
+        //return t => t.Amount < 0;
+        var tParameter = Expression.Parameter(typeof(Transaction), "t");
+        var amountProperty = Expression.Property(tParameter, nameof(Transaction.Amount));
+        var negativeAmount = Expression.LessThan(amountProperty, Expression.Constant(0));
+
+        return Expression.Lambda<Func<Transaction, bool>>(negativeAmount, tParameter);
     }
 
     public static Expression<Func<Transaction, bool>> IsIncomeTransaction()
     {
-        return t => t.Amount >= 0;
+        //return t => t.Amount >= 0;
+        var tParameter = Expression.Parameter(typeof(Transaction), "t");
+        var amountProperty = Expression.Property(tParameter, nameof(Transaction.Amount));
+        var positiveAmount = Expression.GreaterThanOrEqual(amountProperty, Expression.Constant(0));
+
+        return Expression.Lambda<Func<Transaction, bool>>(positiveAmount, tParameter);
+
     }
 }
